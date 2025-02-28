b750806304e63658cc765fee2ea2ee6bf2a13d2d
Khah <khahtech2010@gmail.com>
2025-02-28 15:50:38 +0200

    Initial backend updates

--- models/User.js
+++ models/User.js
@@ -4,10 +4,10 @@ const userSchema = new mongoose.Schema({
   username: { type: String, required: true, unique: true },
   password: { type: String, required: true },
   phoneNumber: { type: String, required: true, unique: true },
-  role: { type: String, enum: ['admin', 'user'], default: 'user' }, // New field
+  role: { type: String, enum: ['admin', 'user'], default: 'user' },
   balance: { type: Number, default: 0 },
   transactions: [{
-    type: { type: String, enum: ['sent', 'received', 'credited'] }, // Added 'credited' for admin actions
+    type: { type: String, enum: ['sent', 'received', 'credited', 'pending-pin'] }, // Added 'pending-pin'
     amount: Number,
     toFrom: String,
     date: { type: Date, default: Date.now }
