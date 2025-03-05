88d9e9b868db579a6977d21a461546fc06bd4744
Khah <khahtech2010@gmail.com>
2025-03-05 08:47:49 +0200

    payment pin

--- index.js
+++ index.js
@@ -14,7 +14,33 @@ mongoose.connect(process.env.MONGO_URI, { useNewUrlParser: true, useUnifiedTopol
 
 // Routes
 app.use('/api', require('./routes/api'));
-app.use(cors({ origin: 'https://khah-334000.web.app' }));
+
+// Define allowed origins for CORS
+const allowedOrigins = [
+  'http://localhost:3000',   // Local development
+  'https://nzubo.net',        // Production
+  'https://nzubo-admin.web.app', // Admin Production
+  'https://kayah.net',        // Production
+  'https://khah-334000.web.app'
+];
+
+// Configure CORS options
+const corsOptions = {
+  origin: (origin, callback) => {
+    // Allow requests with no origin (like mobile apps or curl requests) or from allowed origins
+    if (!origin || allowedOrigins.includes(origin)) {
+      callback(null, true);
+    } else {
+      callback(new Error('Not allowed by CORS'));
+    }
+  },
+  methods: ['GET', 'POST', 'PUT', 'DELETE'],    // Specify allowed HTTP methods
+  credentials: true            // Allow credentials (cookies, authorization headers)
+};
+
+// Apply CORS middleware with the configured options
+app.use(cors(corsOptions));
+// app.use(cors({ origin: 'https://khah-334000.web.app' }));
 
 const PORT = process.env.PORT || 3002;
 app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
