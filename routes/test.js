e17076a2c865ed658907143dc164d49378fa2e76
Khah <khahtech2010@gmail.com>
2025-04-18 00:44:14 +0200

    business signup

--- routes/test.js
+++ routes/test.js
@@ -1,6 +1,6 @@
 const express = require('express');
 const router = express.Router();
-const bcrypt = require('bcrypt');
+const bcrypt = require('bcryptjs');
 const TestBusiness = require('../models/TestBusiness');
 
 router.post('/test/register', async (req, res) => {
