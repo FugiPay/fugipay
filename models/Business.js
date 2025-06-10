19a6c0c5251498b5359766291dc7d472616bdd90
Khah <khahtech2010@gmail.com>
2025-06-10 03:11:10 +0200

    business routes

--- models/Business.js
+++ models/Business.js
@@ -9,7 +9,7 @@ const auditLogSchema = new mongoose.Schema({
     required: true,
     enum: [
       'create', 'update', 'delete', 'kyc_update', 'balance_change', 'login',
-      'pin_reset', 'view_dashboard', 'update_notifications', 'withdrawal_request',
+      'pin_reset', 'pin_reset_request', 'view_dashboard', 'update_notifications', 'withdrawal_request',
       'transaction_received', 'qr_generate', 'withdrawal', 'deposit_verification',
       'set_active', 'tier_update', 'delete-account' // Added delete-account
     ],
