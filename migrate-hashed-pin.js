ed22b6f826f39a1215e3e30a1a3da0a5866270d0
Khah <khahtech2010@gmail.com>
2025-06-20 12:32:00 +0200

    user routes momo

--- migrate-hashed-pin.js
+++ migrate-hashed-pin.js
@@ -1,11 +1,11 @@
 const mongoose = require('mongoose');
 const bcrypt = require('bcryptjs');
 const crypto = require('crypto');
-const User = require('../models/User');
+const User = require('./models/User');
 
 async function migrateHashedPin() {
   try {
-    await mongoose.connect(process.env.MONGODB_URI, {
+    await mongoose.connect('mongodb+srv://KHAH-YAH:01H0EwNnhMYW8zpO@cluster0.1ap41.mongodb.net/Zangena?retryWrites=true&w=majority', {
       useNewUrlParser: true,
       useUnifiedTopology: true,
     });
